36b1efb0d6e9627b48a5440c054d626428199a49
zk <596718356@qq.com>
2021-11-11 19:33:15 +0800

    添加退出

--- src/content/TableData.js
+++ src/content/TableData.js
@@ -244,10 +244,10 @@ export default function BasicTable(props) {
             <div className="ext-title">
               <div className="ext-l">
                 {userinfo && userinfo.userinfo.nickname}
-                <span>
+                <i>
                   <PersonIcon className="a-cursor" color="#fff" fontSize="small" />
-                  退出
-                </span>
+                </i>
+                <span>退出</span>
               </div>
               <div className="ext-m">
                 <img className="title-logo" src="http://h5.57xg.com/logoxg.png" alt="" />
